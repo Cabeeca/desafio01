85e98ee7df851f92b436c09d3a9cdb41cfa1b090
Cabeeca <vettoribmxx@gmail.com>
2022-06-01 17:47:52 -0300

    atual

--- server/database/db.js
+++ server/database/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 
-const db = new Sequelize('df1hr', 'root', 'minhasenha@db', {
+const db = new Sequelize('df1hr', 'vettori', 'minhasenha@db', {
   host: 'localhost',
   dialect: 'mysql'
 })
